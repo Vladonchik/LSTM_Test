4b4f9986d9fe1995ae4582df83ca417ffe259ee8
vladvyshnevskyy <vv1631@my.bristol.ac.uk>
2018-10-24 16:55:31 +0300

    test_sasha's data

---
+++ test_Sasha's_Data.py
@@ -0,0 +1,175 @@
+#import os
+#os.environ["CUDA_VISIBLE_DEVICES"]="2"
+
+import tensorflow as tf
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+
+
+import pandas as pd
+from collections import deque
+import random
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM, CuDNNLSTM, BatchNormalization
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
+from sklearn import preprocessing
+import time
+
+#gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
+#sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=True))
+
+SEQ_LEN = 60  # how long of a preceeding sequence to collect for RNN
+FUTURE_PERIOD_PREDICT = 3  # how far into the future are we trying to predict?
+RATIO_TO_PREDICT = "BTC-USD"
+EPOCHS = 10  # how many passes through our data
+BATCH_SIZE = 64  # how many batches? Try smaller batch if you're getting OOM (out of memory) errors.
+NAME = '{}-SEQ-{}-PRED-{}'.format(SEQ_LEN,FUTURE_PERIOD_PREDICT, int(time.time()))
+
+def classify(current, future):
+    if float(future) > float(current):  # if the future price is higher than the current, that's a buy, or a 1
+        return 1
+    else:  # otherwise... it's a 0!
+        return 0
+
+main_df = pd.DataFrame() # begin empty
+
+file_name = 'Candle_BTCUSD'
+ratio = 'BTC-USD'
+dataset = '{}.csv'.format(file_name)
+df = pd.read_csv(dataset)  # read in specific file
+
+df = df[(df != 0).all(1)]  # drop 0's
+
+df.rename(columns={"closePrice": '{}_close'.format(ratio), "volume": '{}_volume'.format(ratio)}, inplace=True)
+
+df.set_index("startTimeStamp", inplace=True)  # set time as index so we can join them on this shared time
+df = df[['{}_close'.format(ratio), '{}_volume'.format(ratio)]]  # ignore the other columns besides price and volume
+
+if len(main_df)==0:  # if the dataframe is empty
+    main_df = df  # then it's just the current df
+
+
+main_df.fillna(method="ffill", inplace=True)  # if there are gaps in data, use previously known values
+main_df.dropna(inplace=True)
+
+main_df['future'] = main_df['{}_close'.format(RATIO_TO_PREDICT)].shift(-FUTURE_PERIOD_PREDICT)
+main_df['target'] = list(map(classify, main_df['{}_close'.format(RATIO_TO_PREDICT)], main_df['future']))
+
+times = sorted(main_df.index.values)  # get the times
+last_5pct = sorted(main_df.index.values)[-int(0.05*len(times))]  # get the last 5% of the times
+
+validation_main_df = main_df[(main_df.index >= last_5pct)]  # make the validation data where the index is in the last 5%
+main_df = main_df[(main_df.index < last_5pct)]  # now the main_df is all the data up to the last 5%
+
+# print(main_df.head(5))  # how did we do??
+# exit(1)
+
+def preprocess_df(df):
+    df = df.drop("future", 1)  # don't need this anymore.
+
+    for col in df.columns:  # go through all of the columns
+        if col != "target":  # normalize all ... except for the target itself!
+            df[col] = df[col].pct_change()  # pct change "normalizes" the different currencies (each crypto coin has vastly diff values, we're really more interested in the other coin's movements)
+            df.dropna(inplace=True)  # remove the nas created by pct_change
+            df[col] = preprocessing.scale(df[col].values)  # scale between 0 and 1.
+
+    df.dropna(inplace=True)  # cleanup again... jic.
+
+
+    sequential_data = []  # this is a list that will CONTAIN the sequences
+    prev_days = deque(maxlen=SEQ_LEN)  # These will be our actual sequences. They are made with deque, which keeps the maximum length by popping out older values as new ones come in
+
+    for i in df.values:  # iterate over the values
+        prev_days.append([n for n in i[:-1]])  # store all but the target
+        if len(prev_days) == SEQ_LEN:  # make sure we have 60 sequences!
+            sequential_data.append([np.array(prev_days), i[-1]])  # append those bad boys!
+
+    random.shuffle(sequential_data)  # shuffle for good measure.
+
+    buys = []  # list that will store our buy sequences and targets
+    sells = []  # list that will store our sell sequences and targets
+
+    for seq, target in sequential_data:  # iterate over the sequential data
+        if target == 0:  # if it's a "not buy"
+            sells.append([seq, target])  # append to sells list
+        elif target == 1:  # otherwise if the target is a 1...
+            buys.append([seq, target])  # it's a buy!
+
+    random.shuffle(buys)  # shuffle the buys
+    random.shuffle(sells)  # shuffle the sells!
+
+    lower = min(len(buys), len(sells))  # what's the shorter length?
+
+    buys = buys[:lower]  # make sure both lists are only up to the shortest length.
+    sells = sells[:lower]  # make sure both lists are only up to the shortest length.
+
+    sequential_data = buys+sells  # add them together
+    random.shuffle(sequential_data)  # another shuffle, so the model doesn't get confused with all 1 class then the other.
+
+    X = []
+    y = []
+
+    for seq, target in sequential_data:  # going over our new sequential data
+        X.append(seq)  # X is the sequences
+        y.append(target)  # y is the targets/labels (buys vs sell/notbuy)
+
+    return np.array(X), y  # return X and y...and make X a numpy array!
+
+train_x, train_y = preprocess_df(main_df)
+validation_x, validation_y = preprocess_df(validation_main_df)
+
+print('train data: {} validation: {}'.format(len(train_x), len(validation_x)))
+print('Dont buys: {}, buys: {}'.format(train_y.count(0), train_y.count(1)))
+print('VALIDATION Dont buys: {}, buys: {}'.format(validation_y.count(0), validation_y.count(1)))
+
+model = Sequential()
+model.add(CuDNNLSTM(128, input_shape=(train_x.shape[1:]), return_sequences=True))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(CuDNNLSTM(128, return_sequences=True))
+model.add(Dropout(0.1))
+model.add(BatchNormalization())
+
+model.add(CuDNNLSTM(128))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Dense(32, activation='relu'))
+model.add(Dropout(0.2))
+
+model.add(Dense(2, activation='softmax'))
+
+
+opt = tf.keras.optimizers.Adam(lr=0.001, decay=1e-6)
+
+# Compile model
+model.compile(
+    loss='sparse_categorical_crossentropy',
+    optimizer=opt,
+    metrics=['accuracy']
+)
+
+tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+
+filepath = "RNN_Final-{epoch:02d}-{val_acc:.3f}"  # unique file name that will include the epoch and the validation acc for that epoch
+checkpoint = ModelCheckpoint("models/{}.model".format(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')) # saves only the best ones
+
+# Train model
+history = model.fit(
+    train_x, train_y,
+    batch_size=BATCH_SIZE,
+    epochs=EPOCHS,
+    validation_data=(validation_x, validation_y),
+    callbacks=[tensorboard, checkpoint],
+)
+
+# Score model
+score = model.evaluate(validation_x, validation_y, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+# Save model
+model.save("models/{}".format(NAME))
